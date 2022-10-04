250d91aae6c145ec00b634481d850150f26f43ac
GABRIEL DE SOUSA <gabrielsousa@univille.br>
2022-10-04 13:45:45 -0300

    springboot

--- modulo3spring/novostalentos/src/main/java/br/univille/novostalentos/controller/HomeController.java
+++ modulo3spring/novostalentos/src/main/java/br/univille/novostalentos/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.univille.novostalentos.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,8 +17,6 @@ public class HomeController {
     public ModelAndView index(){
         //return "eu nao acredito... java é lindo ❤";
         contador++;
-
-        return new ModelAndView("home/index", "valor", contador);
+        return new ModelAndView("home/index","valor",contador);
     }
-
 }
