47e44928064bb0b33bac6488b9359646a794a712
bamgabsz <bamgabsz@gmail.com>
2022-11-11 16:57:07 -0300

    spring

--- modulo3spring/novostalentos/src/main/java/br/univille/novostalentos/controller/ClienteController.java
+++ modulo3spring/novostalentos/src/main/java/br/univille/novostalentos/controller/ClienteController.java
@@ -1,17 +1,21 @@
 package br.univille.novostalentos.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.univille.novostalentos.entity.Cliente;
+import br.univille.novostalentos.service.CidadeService;
 import br.univille.novostalentos.service.ClienteService;
 
 @Controller
@@ -20,6 +24,8 @@ public class ClienteController {
     
     @Autowired
     private ClienteService service;
+    @Autowired
+    private CidadeService cidadeService;
 
     @GetMapping
     public ModelAndView index(){
@@ -29,21 +35,37 @@ public class ClienteController {
     @GetMapping("/novo")
     public ModelAndView novo(){
         var cliente = new Cliente();
-        return new ModelAndView("cliente/form","cliente",cliente);
-    }
-    @PostMapping(params = "form")
-    public ModelAndView save(Cliente cliente){
-        
-        service.save(cliente);
-
-        return new ModelAndView("redirect:/clientes");
+        var listaCidades = cidadeService.getAll();
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("cliente",cliente);
+        dados.put("listaCidades",listaCidades);
+        return new ModelAndView("cliente/form", dados);
     }
     @GetMapping("/alterar/{id}")
     public ModelAndView alterar(@PathVariable("id") long id){
         var umCliente = service.findById(id);
-
-        return new ModelAndView("cliente/form","cliente",umCliente);
+        var listaCidades = cidadeService.getAll();
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("cliente",umCliente);
+        dados.put("listaCidades",listaCidades);
+        return new ModelAndView("cliente/form",dados);
     }
+    @PostMapping(params = "form")
+    public ModelAndView save(@Valid Cliente cliente,
+                            BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            var listaCidades = cidadeService.getAll();
+            HashMap<String,Object> dados = new HashMap<>();
+            dados.put("cliente",cliente);
+            dados.put("listaCidades",listaCidades);
+            return new ModelAndView("cliente/form",dados);
+        }
+        service.save(cliente);
+
+        return new ModelAndView("redirect:/clientes");
+    }
+    
     @GetMapping("/delete/{id}")
     public ModelAndView delete(@PathVariable("id") long id){
 
--- modulo3spring/novostalentos/src/main/java/br/univille/novostalentos/entity/Usuario.java
+++ modulo3spring/novostalentos/src/main/java/br/univille/novostalentos/entity/Usuario.java
@@ -1,23 +1,18 @@
 package br.univille.novostalentos.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Cidade {
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(length = 1000,unique = true)
     private String nome;
-
-    public String toString(){
-        return getNome();
-    }
-
+    private String senha;
+    
     public long getId() {
         return id;
     }
@@ -30,5 +25,13 @@ public class Cidade {
     public void setNome(String nome) {
         this.nome = nome;
     }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
     
 }
